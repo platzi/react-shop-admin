04842bc9004f1157893f1aeb76c6cebccdc94954
Oscar Barajas <oscar@gndx.dev>
2021-12-07 14:46:29 -0500

    :art: [ADD] Dashboard

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,14 @@
+import MainLayout from '@layout/MainLayout';
 import '@styles/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </>
+  );
 }
 
 export default MyApp;
