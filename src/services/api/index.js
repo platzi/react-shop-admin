1d2a56dfffca863c99bb0eeb0dbf99a2f9d292f5
Oscar Barajas <oscar@gndx.dev>
2021-12-07 16:29:55 -0500

    :zap: [ADD] API Service

---
+++ src/services/api/index.js
@@ -0,0 +1,27 @@
+const API = process.env.NEXT_PUBLIC_API_URL;
+const VERSION = process.env.NEXT_PUBLIC_API_VERSION;
+
+const endPoints = {
+  auth: {
+    login: `${API}/api/${VERSION}/auth/login`,
+    profile: `${API}/api/${VERSION}/auth/profile`,
+  },
+  products: {
+    getProduct: (id) => `${API}/api/${VERSION}/products/${id}/`,
+    getProducts: (limit, offset) => `${API}/api/${VERSION}/products?limit=${limit}&offset=${offset}`,
+    addProducts: `${API}/api/${VERSION}/products`,
+    updateProducts: (id) => `${API}/api/${VERSION}/products/${id}/`,
+    deleteProducts: (id) => `${API}/api/${VERSION}/products/${id}/`,
+  },
+  categories: {
+    getCategoriesList: `${API}/api/${VERSION}/categories/`,
+    addCategory: `${API}/api/${VERSION}/categories/`,
+    getCategoryItems: (id) => `${API}/api/${VERSION}/categories/${id}/products/`,
+    updateCategory: (id) => `${API}/api/${VERSION}/categories/${id}/`,
+  },
+  files: {
+    addImage: `${API}/api/${VERSION}/files/upload/`,
+  },
+};
+
+export default endPoints;
