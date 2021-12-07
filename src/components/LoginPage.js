59c55c7b99795463b25acc16d3cdd207cacd83de
Oscar Barajas <oscar@gndx.dev>
2021-12-07 16:07:47 -0500

    :zap: [ADD] Login

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,6 +1,18 @@
+import { useRef } from 'react';
 import { LockClosedIcon } from '@heroicons/react/solid';
 
 export default function LoginPage() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const submitHanlder = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    console.log(email, password);
+  };
+
   return (
     <>
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -9,7 +21,7 @@ export default function LoginPage() {
             <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
           </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
+          <form className="mt-8 space-y-6" onSubmit={submitHanlder}>
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="rounded-md shadow-sm -space-y-px">
               <div>
@@ -24,6 +36,7 @@ export default function LoginPage() {
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Email address"
+                  ref={emailRef}
                 />
               </div>
               <div>
@@ -38,6 +51,7 @@ export default function LoginPage() {
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Password"
+                  ref={passwordRef}
                 />
               </div>
             </div>
---
+++ src/pages/login.js
@@ -0,0 +1,5 @@
+import LoginPage from '@components/LoginPage';
+
+export default function Login() {
+  return <LoginPage />;
+}
