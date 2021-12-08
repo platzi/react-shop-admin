96bce1f7d27bd278ba9811da13d6f4ef22003fc6
Oscar Barajas <oscar@gndx.dev>
2021-12-08 16:18:27 -0500

    :zap: [ADD] Axios Headers

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -26,7 +26,11 @@ function useProvideAuth() {
     };
     const { data: access_token } = await axios.post(endPoints.auth.login, { email, password }, options);
     if (access_token) {
-      Cookie.set('token', access_token.access_token, { expires: 5 });
+      const token = access_token.access_token;
+      Cookie.set('token', token, { expires: 5 });
+      axios.defaults.headers.Authorization = `Bearer ${token}`;
+      const { data: user } = await axios.get(endPoints.auth.profile);
+      setUser(user);
     }
   };
 
