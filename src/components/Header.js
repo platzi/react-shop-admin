8bac3a958192f12bb7bd11811f0710bd8d383d0f
Oscar Barajas <oscar@gndx.dev>
2021-12-10 15:28:21 -0500

    :zap: [ADD] Logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,6 @@ const navigation = [
 const userNavigation = [
   { name: 'Your Profile', href: '#' },
   { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' },
 ];
 
 function classNames(...classes) {
@@ -81,15 +80,9 @@ export default function Header() {
                         leaveTo="transform opacity-0 scale-95"
                       >
                         <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                          {userNavigation.map((item) => (
-                            <Menu.Item key={item.name}>
-                              {({ active }) => (
-                                <a href={item.href} className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
-                                  {item.name}
-                                </a>
-                              )}
-                            </Menu.Item>
-                          ))}
+                          <button onClick={() => auth.logout()} className="block px-4 py-2 text-sm text-gray-700">
+                            Logout
+                          </button>
                         </Menu.Items>
                       </Transition>
                     </Menu>
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -34,8 +34,16 @@ function useProvideAuth() {
     }
   };
 
+  const logout = () => {
+    Cookie.remove('token');
+    setUser(null);
+    delete axios.defaults.headers.Authorization;
+    window.location.href = '/login';
+  };
+
   return {
     user,
     signIn,
+    logout,
   };
 }
