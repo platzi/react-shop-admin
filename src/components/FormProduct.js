5cfa3919cca19408f421748e3bc931b83e5ff7cb
Oscar Barajas <oscar@gndx.dev>
2021-12-10 15:40:13 -0500

    [UPDATE] Next Config

--- src/components/FormProduct.js
+++ src/components/FormProduct.js
@@ -1,5 +1,5 @@
 import { useRef } from 'react';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 import { addProduct, updateProduct } from '@services/api/products';
 
 export default function FormProduct({ setOpen, setAlert, product }) {
